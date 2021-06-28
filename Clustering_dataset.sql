8d1b6c43e0e0bdeac3e7c49a4144780bba357951
IliasMarkopoulos <86572580+IliasMarkopoulos@users.noreply.github.com>
2021-06-28 22:08:15 +0300

    Delete Clustering_dataset.sql

--- Clustering_dataset.sql
+++
@@ -1,6 +0,0 @@
-select user_id, sum(basket) as total_amount, avg(basket) as avg_amount, count(order_id) as Breakfast_orders, 
-count(CAST(submit_dt AS DATE)) as no_days, sum(basket)/count(order_id) as amount_per_order
-
-from  `bi-2019-test.ad_hoc.orders_jan2021`
-where cuisine_parent = 'Breakfast'
-group by user_id
